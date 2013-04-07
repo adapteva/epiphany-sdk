c28f2e050583d1fb2cc2b4a8226782080b72dd3a
ysapir <yanivsap@hotmail.com>
2013-04-07 19:36:28 -0400

    4.13.03.30 update 3

--- build-sdk.sh
+++ build-sdk.sh
@@ -2,51 +2,77 @@
 
 set -e
 
-ESDK=${EPIPHANY_HOME}
+REV="4.13.03.30"
+ESDKPATH="/opt/adapteva"
+EPIPHANY_HOME="${ESDKPATH}/esdk"
+ESDK="${ESDKPATH}/esdk.${REV}"
 ARCH='armv7l'
-HOST=${ESDK}/tools/host.${ARCH}
+HOSTNAME="host.${ARCH}"
+HOST="${ESDK}/tools/${HOSTNAME}"
+GNUNAME="e-gnu.${ARCH}"
+GNU="${ESDK}/tools/${GNUNAME}"
 BSP='zed_E64G4_512mb'
 ESDK_LIBS='../epiphany-libs'
 
-if [ ! -d "${ESDK}/tools/host/lib" ]; then
-	mkdir -p ${HOST}/lib
-	mkdir -p ${HOST}/include
-	mkdir -p ${HOST}/bin
-fi
+export PATH="${EPIPHANY_HOME}/tools/e-gnu/bin:${EPIPHANY_HOME}/tools/host/bin:${PATH}"
 
-if [ ! -d "${ESDK}/bsps" ]; then
-	mkdir -p ${ESDK}/bsps
-fi
+echo "==============================================="
+echo "| NOTE: The default BSP is set to ${BSP}      |"
+echo "| Please make sure it matches your system,    |"
+echo "| or chenge the settings in this build script |"
+echo "==============================================="
 
+# Create the SDK tree and set default symlinks
+echo "Creating the eSDK directory tree..."
+mkdir -p ${ESDK}
+ln -sTf "esdk.${REV}" ${ESDKPATH}/esdk
+mkdir -p ${ESDK}/bsps
+mkdir -p ${ESDK}/docs
+mkdir -p ${ESDK}/examples
+mkdir -p ${ESDK}/tools
+
+mkdir -p ${HOST}
+mkdir -p ${GNU}
+ln -sTf ${HOSTNAME} ${ESDK}/tools/host
+ln -sTf ${GNUNAME} ${ESDK}/tools/e-gnu
+
+mkdir -p ${HOST}/lib
+mkdir -p ${HOST}/include
+mkdir -p ${HOST}/bin
+
+
+# Check prerequisites
 if [ ! -d "${ESDK}/tools/e-gnu/epiphany-elf/lib" ]; then
-	echo "Please install the Epiphany GNU tools suite first at ${ESDK}/tools/e-gnu!"
-	exit
+	echo "Please install the Epiphany GNU tools suite first at ${GNU}"
+	exit 1
 fi
 
 if [ ! -d "${ESDK_LIBS}" ]; then
-#	echo "Please make sure the epiphany-libs repo is placed correctly!"
 	echo "ERROR: Can't find the epiphany-libs repository!"
 	exit 1
 fi
 
 
+# Build the eSDK libraries from epiphany-libs repo
 echo "Building eSDK libraries..."
 pushd ${ESDK_LIBS} >& /dev/null
 ./build-libs.sh
 popd >& /dev/null
 
+
+# Install components
 echo "Installing eSDK components..."
 
 # Install the documentation and examples
-cp -Rd docs     ${ESDK}/docs/
-cp -Rd examples ${ESDK}/examples/
+cp -Rd docs     ${ESDK}
+cp -Rd examples ${ESDK}
 
 
 pushd ${ESDK_LIBS} >& /dev/null
 
 # Install the current BSP
-cp -Rd bsps/${BSP} ${ESDK}/bsps/
-ln -sTf ${BSP} ${ESDK}/bsps/bsp
+cp -Rd bsps/${BSP} ${ESDK}/bsps
+ln -sTf ${BSP} ${ESDK}/bsps/current
 
 # Install the XML parser library
 cd src/e-xml
@@ -88,4 +114,6 @@ cd ../../
 
 popd >& /dev/null
 
+
+# Any special operations here...
 
