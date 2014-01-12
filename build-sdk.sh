b6b0e438dc734400feb7b92b37cd641631e749ad
Jeremy Bennett <jeremy.bennett@embecosm.com>
2014-01-12 17:40:24 +0100

    Add missing (but implicit) license header, .gitignore and ChnageLog.
    	* .gitignore: Created.
    	* build-sdk.sh: Added GPL header, rather than leaving as implicit,
    	use Intel rather than ARM as architecture.
    	* ChangeLog: Created.

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Copyright (C) 2009 - 2013 Adapteva Inc.
+
+# Contributor Yaniv Sapir <ysapir@adapteva.com>
+# Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
+
+# This file is a script to build key elements of the Epiphany SDK
+
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.          
+
 set -e
 
 # Path to location of eSDK installation
@@ -11,7 +31,8 @@ ESDKPATH="${PWD}/.."     # In user account, adjacent directory
 REV="5.13.09.10"
 
 # Host machine architecture
-ARCH="armv7l"
+# ARCH="armv7l"
+ARC="x86_64"
 
 # List if available BSPs and default BSP
 BSPS="zed_E16G3_512mb zed_E64G4_512mb parallella_E16G3_1GB"
