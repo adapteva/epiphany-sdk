85d7c00c7eb9cb1153d9869dbb0fc93841caf1ed
Ola Jeppsson <ola@adapteva.com>
2015-02-23 06:22:00 +0100

    travis: hack: Keep build alive with dummy loop
    * .travis.sh: New file. Add dummy ping loop to keep build
    alive.
    * .travis.yml: Call .travis.sh
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

---
+++ .travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Abort on Error
+set -e
+
+export PING_SLEEP=30s
+
+dump_output() {
+	echo Tailing the last 500 lines of output:
+	tail -500 $(find logs -type f -name "build*.log")
+}
+error_handler() {
+	echo ERROR: An error was encountered with the build.
+	dump_output
+	exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do echo; echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+./sdk/build-epiphany-sdk.sh -j 1
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
